9aebc109858cb41da52874ca365ce30e09c576a0
Hanna <hanna.botar@teamnet.ro>
2018-01-22 19:22:48 +0200

    first commit

---
+++ app/src/main/java/com/example/hannabotar/artquiz/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.hannabotar.artquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_NAME = "quiz_user_name";
+
+    Button startButton;
+    EditText nameInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.start_button);
+        startButton.setEnabled(false);
+
+        nameInput = findViewById(R.id.name_input);
+        nameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() > 0) {
+                    startButton.setEnabled(true);
+                } else {
+                    startButton.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    public void clickStart(View view) {
+        // navigate to scrollable questions
+        Log.i("MainActivity","Start button clicked");
+
+        String name = nameInput.getText().toString();
+        Intent intent = new Intent(this, ScrollableQuizActivity.class);
+        intent.putExtra(EXTRA_NAME, name);
+        startActivity(intent);
+    }
+}
